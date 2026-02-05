04e61f2781fe4af793469cf7e29730fece538df4
GUJUGIT <next39742@edu.nextkbh.dk>
2026-02-05 13:56:34 +0100

    opgave 3

--- indexto.js
+++ indexto.js
@@ -4,9 +4,24 @@ let elitesports = [
     { name: "Sebastian", age: 20, course: "Elitetennisspiller" },
 ];
 
-let eliteListDiv = document.querySelector(".elite-sport");
+let eliteListDiv = document.querySelector(".elite-sport"); //vælger element
+
+const eliteUl = document.createElement("ul"); //skaber
+
+eliteListDiv.insertAdjacentElement("afterbegin", eliteUl); //indsætter
 
 elitesports.forEach(function(elitesport) {
-    console.log(elitesport);
+    //console.log(elitesport);
+    /*html*/
+   let li = 
+   `
+    <li>
+        <h2 class="course__header">Course${elitesport.course} </h2>
+        <p>Deltagers navn ${elitesport.name} og alder: ${elitesport.age} </p>
+    </li>
+    `
+
+    eliteUl.insertAdjacentHTML("afterbegin", li); //indsætter også
 });
+
 

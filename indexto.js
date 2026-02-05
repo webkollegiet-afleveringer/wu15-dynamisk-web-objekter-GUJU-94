2edf1d124c0f215ab8a659ede5f78d7ae59584c8
GUJUGIT <next39742@edu.nextkbh.dk>
2026-02-05 13:33:38 +0100

    opgave 3

---
+++ indexto.js
@@ -0,0 +1,12 @@
+let elitesports = [
+    { name: "Josephine", age: 19, course: "Elitesvømmer" },
+    { name: "Clara", age: 18, course: "Elitegymnast" },
+    { name: "Sebastian", age: 20, course: "Elitetennisspiller" },
+];
+
+let eliteListDiv = document.querySelector(".elite-sport");
+
+elitesports.forEach(function(elitesport) {
+    console.log(elitesport);
+});
+

37803e6bd6c6acb5cde89222f5f7586db64b025c
GUJUGIT <next39742@edu.nextkbh.dk>
2026-02-05 10:28:27 +0100

    opgave 1

---
+++ index.js
@@ -0,0 +1,14 @@
+let dog = {
+    name: "Molly",
+    age: "5",
+    adress: "Roskilde",
+    color: "Brown"
+}
+
+console.log(dog.name); //Molly
+console.log(dog.adress); //Roskilde
+
+console.log(dog["age"]); //man kan lave på begge disse måder
+console.log(dog["color"]); //disse to måder med molly og roskilde er det samme som age og color
+
+
